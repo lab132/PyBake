c9d08c25a83fe0256c8047cd0b20cd8abe17824d
Marvin Pohl <mp078@hdm-stuttgart.de>
2015-03-14 22:19:10 +0100

    Simple client server connection on localhost

--- code/PyBake.py
+++ code/PyBake.py
@@ -2,4 +2,41 @@
 Baking py since 2015
 '''
 
-print('Hello World')
+import socket
+from sys import argv
+
+serveradress = ("127.0.0.1",1337)
+
+
+commands = {}
+
+def command(name):
+    def commandwrapper(f):
+        commands[name] = f
+        return f
+    return commandwrapper
+
+@command("server")
+def server():
+
+    print("Starting up Server")
+
+    s = socket.socket()
+    #s.connect()
+    s.bind(serveradress)
+    s.listen(5)
+
+    print("Server is listening on port {0}".format(serveradress[1]))
+
+    while True:
+        connection, address = s.accept()
+        print('Connected with ' + address[0] + ':' + str(address[1]))
+
+@command("client")
+def client():
+    
+    s = socket.socket()
+    s.connect(serveradress)
+
+if __name__ == "__main__":
+    commands[argv[1]]()
