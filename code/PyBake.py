54e61d9f261d57ab8715c3180e66123ea30f5658
Manuel Maier <mjmaier@gmx.de>
2015-03-18 14:55:46 +0100

    Add  command to run user recipe.py and get 'ingredients' from it. Warning: Still very experimental.

--- code/PyBake.py
+++ code/PyBake.py
@@ -5,20 +5,19 @@ Baking py since 2015
 import os
 import sys
 
-# Insert current working dir to the sys path so we can import the crumble in there.
+# Insert current working dir to the sys path so we can import python modules from there.
 sys.path.insert(0, os.getcwd())
-
-try:
-    import crumble
-except:
-    print("Unable to load crumble.py")
-    raise
 
 import socket
 import importlib
 from urllib.request import urlopen
 import json
 import pathlib
+from copy import deepcopy
+from importlib import import_module
+
+# Relative path to the user-defined script.
+userRecipe = "recipe"
 
 class ServerConfig:
     """docstring"""
@@ -32,6 +31,62 @@ class ServerConfig:
 
     def __repr__(self):
         return "Server config {0}".format(self)
+
+class Platform:
+    """Object describing a platform such as 'Windows 64 bit VisualStudio2013 Debug'."""
+
+    def __init__(self, *, name=None, detailed_name=None, bits=64, generator=None, config="Debug", user_data=None):
+        self.name = name
+        self.detailed_name = detailed_name
+        self.bits = bits
+        self.generator = generator
+        self.config = config
+        self.user_data = user_data
+
+    def isValid(self):
+        return self.name is not None or self.detailed_name is not None
+
+    def __str__(self):
+        if self.detailed_name is not None:
+            return self.detailed_name
+        return self.name
+
+    def __repr__(self):
+        return "<{0.name} ({0.detailed_name}) {0.bits} bit {0.generator} {0.config}: {0.user_data}".format(self)
+
+    # Represents all platforms.
+    All = None # Is initialized after the declaration of this class
+
+    @staticmethod
+    def FromDict(desc):
+        result = Platform()
+        result.name          = desc.get("name", result.name)
+        result.detailed_name = desc.get("detailed_name", result.detailed_name)
+        result.bits          = desc.get("bits", result.bits)
+        result.generator     = desc.get("generator", result.generator)
+        result.config        = desc.get("config", result.config)
+        result.user_data     = desc.get("user_data", result.user_data)
+        return result
+
+Platform.All = Platform(name="all")
+
+class Pastry:
+    """File meta data."""
+    def __init__(self, path):
+        self.path = path
+
+class Ingredient:
+    """A wrapper for an existing file that has tags attached to it."""
+    def __init__(self, path, *, platform=Platform.All, tags=[]):
+        self.path = path
+        self.platform = platform
+        self.tags = tags
+
+    def __str__(self):
+        return "{0.path.name}<{0.platform}>{0.tags}".format(self)
+
+    def __repr__(self):
+        return "{}<{}>{}".format(repr(self.path.name), repr(self.platform), repr(self.tags))
 
 Path = pathlib.Path
 
@@ -66,18 +121,30 @@ def server():
         p = os.listdir(".")
         print(p)
         return jsonify({"values" : p})
-
+    import crumble
     app.run(debug=True, host=crumble.server.host, port=crumble.server.port)
 
 @command("client")
 def client():
+    import crumble
     response = json.load(urlopen("{0}/list_packages".format(crumble.server)))
     print(response)
 
 @command("oven")
 def oven():
-    # This command used an ingredients.json and a recipe.py file to generate a crumble (server-side package)
-    pass
+    """
+    This command uses a recipe.py file to generate a pastry.json (server-side package/crumble).
+    """
+    global recipes
+    recipes = []
+
+    # Import recipe.py, which should be defined by the user.
+    # This will register functions in the global variable `recipes`.
+    recipe = import_module(userRecipe)
+
+    print("Ingredients:")
+    for ing in recipe.ingredients:
+        print("  {0}".format(repr(ing)))
 
 if __name__ == "__main__":
     commands[sys.argv[1]]()
--- samples/serversideCrumbleDescription/recipe.py
+++ samples/serversideCrumbleDescription/recipe.py
@@ -1,24 +1,83 @@
-# Generates the pastry.json.
+# This script is used to attach tags to files.
 from PyBake import *
 
-tagRoutes = {
-    "headers" : "Code/Engine",
-    "runtime-binaries" : "Output/{operatingSystem}{bits}{generator}{config}/Bin/*.(dll|pdb)",
-    "compile-binaries" : "Output/{operatingSystem}{bits}{generator}{config}/Lib/*.lib",
-}
+# [Required] Will contain all tagged files.
+ingredients = []
 
-moduleNames = [
-    "ezFoundation", "ezCore"
-]
+def extract_platform(path):
+    """Parses a platform object from something like 'WinVs2013Debug32'."""
+    name = path.name
+    print("Extracting platform from '{0}'".format(name))
 
-@packageTag("headers")
-def getHeaderFiles():
-    return [ "Code/Engine/{0}".format(x) for x in Path("Code/Engine/").iter_dir() ]
+    p = Platform()
 
-def getFiles(tag, platform, modules=None):
-    if modules is None:
-        modules = moduleNames
+    # Platform name.
+    i_name = 1
+    while name[i_name].islower():
+        i_name += 1
+    p.name = name[:i_name]
 
-    if tag == "headers":
-        return Path(tagRoutes["headers"])
+    # Platform generator.
+    i_gen = i_name + 1
+    while name[i_gen].islower():
+        i_gen += 1
+    p.generator = name[i_name:i_gen]
 
+    # Platform config
+    i_cfg = i_gen + 1
+    while name[i_cfg].islower():
+        i_cfg += 1
+    p.config = name[i_gen:i_cfg]
+
+    # Platform bits.
+    p.bits = int(name[-2:]) # Last 2 characters are the bits
+
+    return p
+
+
+ezEngineRoot = Path(".").resolve()
+
+# Path for all header files.
+ezEnginePath_Headers = ezEngineRoot / "Code" / "Engine"
+ezEnginePath_Headers.resolve()
+
+# Path for dll, pdb, and so files.
+ezEnginePath_Bin     = ezEngineRoot / "Output" / "Bin"
+ezEnginePath_Bin.resolve()
+
+# Path for lib files.
+ezEnginePath_Lib     = ezEngineRoot / "Output" / "Lib"
+ezEnginePath_Lib.resolve()
+
+print("headers")
+tags = [ "header-file" ]
+files = ezEnginePath_Headers.rglob("*.h")
+ingredients.extend([ Ingredient(h, tags=["header-file"]) for h in files ])
+
+
+print("runtime_files")
+tags = [ "rt-file" ]
+path = ezEnginePath_Bin.glob("*")
+for subpath in path:
+    if not subpath.is_dir() or subpath.match("*Tests"):
+        continue
+    files = []
+    p = extract_platform(subpath)
+
+    files.extend(subpath.rglob("ez*.dll"))
+    files.extend(subpath.rglob("ez*.pdb"))
+    files.extend(subpath.rglob("ez*.so"))
+    ingredients.extend([ Ingredient(x, platform=p, tags=tags) for x in files ])
+
+
+print("compiletime_files")
+tags = [ "ct-file" ]
+path = ezEnginePath_Lib.glob("*")
+for subpath in path:
+    if not subpath.is_dir():
+        continue
+    files = []
+    p = extract_platform(subpath)
+
+    files.extend(subpath.rglob("ez*.lib"))
+    ingredients.extend([ Ingredient(x, platform=p, tags=tags) for x in files ])
