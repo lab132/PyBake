bdf6dcc329cd26adb39efe3ea3920b41a028c413
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-04-09 09:41:15 +0100

    Oven's argparse declaration is now in oven.py

--- PyBake/__main__.py
+++ PyBake/__main__.py
@@ -27,11 +27,6 @@ description = textwrap.dedent(
   A single dependency is referred to as a crumble.
   """)
 
-ovenDescription = textwrap.dedent(
-  """
-  Tool to create crumbles.
-  """)
-
 serverDescription = textwrap.dedent(
   """
   Sets up a server for crumble management
@@ -43,14 +38,6 @@ def execute_shop(args):
   log.debug(args)
   from PyBake import shop
   return shop.run(**vars(args))
-
-
-def execute_oven(args):
-  """Execute the `oven` command."""
-  log.info("Executing oven")
-  from PyBake import oven
-  args.compression = zipCompressionLookup[args.compression]
-  return oven.run(**vars(args))
 
 # Main Parser
 # ===========
@@ -76,27 +63,6 @@ for module in pyBakeSubmodules:
   if hasattr(importedModule, "moduleManager"):
     importedModule.moduleManager.createSubParser(subparsers)
 
-# OvenParser
-# ==========
-ovenParser = subparsers.add_parser("oven", help=ovenDescription, description=ovenDescription)
-
-ovenParser.add_argument("recipes_script",
-                        type=Path,
-                        nargs="?",
-                        default=Path("recipes.py"),
-                        help="Path to the recipes script. Default: 'recipes.py'")
-ovenParser.add_argument("-o", "--output", type=Path, default=Path(".pastries"),
-                        help="The directory to store the pastries in. Defaults to '.pastries'.")
-ovenParser.add_argument("-d", "--working-dir",
-                        type=Path,
-                        nargs="?",
-                        default=Path.cwd(),
-                        help="The working directory when executing the `recipes_script`. Defaults to the current working dir.")
-ovenParser.add_argument("-c", "--compression",
-                        choices=zipCompressionLookup.keys(),
-                        default="deflated",
-                        help="The compression method used to create a pastry.")
-ovenParser.set_defaults(func=execute_oven)
 
 
 # ServerParser
