9372582011dd33cea86d8a66c75e4b3b8894f242
Manuel Maier <mjmaier@gmx.de>
2015-04-17 08:24:25 +0200

    :lipstick: Extracted download of pastries in basket to separate function.

--- PyBake/basket.py
+++ PyBake/basket.py
@@ -16,6 +16,38 @@ def get_location_path(loc):
   return lookup[loc]
 
 
+def downloadPastries(menu, pastries, server):
+  import requests
+  for pastry in pastries:
+    if menu.exists(pastry):
+      log.dev("Pastry already exists locally: {}".format(pastry))
+      continue
+    with LogBlock("Requesting {}".format(pastry)):
+      response = requests.post("{}/get_pastry".format(server),
+                               data=dict(pastry),
+                               stream=True)
+      if not response.ok:
+        log.error("Request failed:\n{}".format(response.text))
+        return
+
+      size = 1024
+      if "content-length" in response.headers:
+        size = int(response.headers["content-length"])
+
+      out_path = menu.makePath(pastry)
+      log.info("Saving to: {}".format(out_path.as_posix()))
+
+      with ByteProgressListener(maxSize=size) as progress:
+        with out_path.open("wb") as out_file:
+          chunk_size = 1024 * 4  # 4 KiB at a time.
+          for chunk in response.iter_content(chunk_size):
+            out_file.write(chunk)
+            progress(chunk_size)
+      menu.add(pastry)
+      log.success("Pastry received.")
+
+
+
 class ByteProgressListener:
   """
   Takes care of logging some bytes progressing.
@@ -73,40 +105,12 @@ def run(*, location, shopping_list="shoppingList", **kwargs):
     import requests
 
     shopping_list = import_module(shopping_list)
-    server_config = try_getattr(shopping_list, ("server_config", "server"), raise_error=True)
+    server = try_getattr(shopping_list, ("server_config", "server"), raise_error=True)
     pastries = try_getattr(shopping_list, ("pastries", "pastry"), raise_error=True)
 
     pastries_root = get_location_path(location) / Menu.defaultPastriesDirPath
     pastries_root.safe_mkdir(parents=True)
     menu = Menu(pastries_root)
     menu.load()
-
-    for pastry in pastries:
-      if menu.exists(pastry):
-        log.dev("Pastry already exists locally: {}".format(pastry))
-        continue
-      with LogBlock("Requesting {}".format(pastry)):
-        response = requests.post("{}/get_pastry".format(server_config),
-                                 data=dict(pastry),
-                                 stream=True)
-        if not response.ok:
-          log.error("Request failed:\n{}".format(response.text))
-          return
-
-        size = 1024
-        if "content-length" in response.headers:
-          size = int(response.headers["content-length"])
-
-        out_path = menu.makePath(pastry)
-        log.info("Saving to: {}".format(out_path.as_posix()))
-
-        with ByteProgressListener(maxSize=size) as progress:
-          with out_path.open("wb") as out_file:
-            chunk_size = 1024 * 4  # 4 KiB at a time.
-            for chunk in response.iter_content(chunk_size):
-              out_file.write(chunk)
-              progress(chunk_size)
-        menu.add(pastry)
-        log.success("Pastry received.")
-
+    downloadPastries(menu, pastries, server)
     menu.save()
