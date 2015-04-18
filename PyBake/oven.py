05ce3d011cb5a080f8773aaf2af04da924026da9
Marvin Pohl <mp078@hdm-stuttgart.de>
2015-04-18 15:09:41 +0100

    :lipstick: Oven command generation is now in ovenCommand.py

--- PyBake/oven.py
+++ PyBake/oven.py
@@ -8,47 +8,6 @@ import json
 import zipfile
 import textwrap
 
-class OvenModuleManager:
-  """Manager class for the oven, registering the argparse commands"""
-  # OvenParser
-  # ==========
-
-  longDescription = textwrap.dedent(
-  """
-  Tool to create crumbles.
-  """)
-
-  def createSubParser(self, subParsers):
-    ovenParser = subParsers.add_parser("oven", help=self.longDescription, description=self.longDescription)
-
-    ovenParser.add_argument("recipes_script",
-                            type=Path,
-                            nargs="?",
-                            default=Path("recipes.py"),
-                            help="Path to the recipes script. Default: 'recipes.py'")
-    ovenParser.add_argument("-o", "--output", type=Path, default=Path(".pastries"),
-                            help="The directory to store the pastries in. Defaults to '.pastries'.")
-    ovenParser.add_argument("-d", "--working-dir",
-                            type=Path,
-                            nargs="?",
-                            default=Path.cwd(),
-                            help="The working directory when executing the `recipes_script`. Defaults to the current working dir.")
-    ovenParser.add_argument("-c", "--compression",
-                            choices=zipCompressionLookup.keys(),
-                            default="deflated",
-                            help="The compression method used to create a pastry.")
-    ovenParser.set_defaults(func=execute_oven)
-
-moduleManager = OvenModuleManager()
-
-
-
-def execute_oven(args):
-  """Execute the `oven` command."""
-  log.info("Executing oven")
-  from PyBake import oven
-  args.compression = zipCompressionLookup[args.compression]
-  return oven.run(**vars(args))
 
 class Pot:
   """Manages new pastries when processing recipes."""
